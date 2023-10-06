49c8806521d3376f78baf30d7b85cf9873811f1a
Farhan-x001 <115449922+Farhan-x001@users.noreply.github.com>
2023-10-06 21:46:40 +0530

    we got cabin

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,26 @@
 import Topbar from "./components/topbar/Topbar";
 import Homepage from "./pages/homepage/Homepage";
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Startups from "./pages/startups/Startups";
 import Cabin from "./pages/cabin/Cabin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   const currentUser = true;
   return (
     <Router>
-      <Topbar /> 
+      <Topbar />
       <div>
-        
         <Switch>
           <Route exact path="/">
             <Homepage />
-            <Startups/>
-            <Cabin/>
-            {/* <CardGroup/>
-            <CandidatesPlaced/>
-            <CompanysMarquee/>
-            <Feedback/> */}
           </Route>
-          {/* <Route path="/cabin">{currentUser ? <Cabin /> : <Login />}</Route>
-          <Route path="/startups">{currentUser ? <Startups /> : <Login />}</Route>
-          <Route path="/posts">
-            <Homepage />
+          <Route path="/startups">
+            <Startups />
           </Route>
-          <Route path="/register">
-            {currentUser ? <Homepage /> : <Register />}
+          <Route path="/cabin">
+            <Cabin />
           </Route>
-          <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>
-          <Route path="/post/:id">
-            <Single />
-          </Route>
-          <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
-          <Route path="/settings">
-            {currentUser ? <Settings /> : <Login />}
-          </Route> */}
+          {/* Add other routes as needed */}
         </Switch>
       </div>
     </Router>
